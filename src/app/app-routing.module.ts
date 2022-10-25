7232d54f97c8aad84268594ab361de2bd4d2e389
pklein2022 <111322242+pklein2022@users.noreply.github.com>
2022-10-24 20:26:59 -0500

    added routing and started chore list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+import { ChoreDetailComponent } from './chores/chore-detail/chore-detail.component';
+import { ChoreListComponent } from './chores/chore-list/chore-list.component';
+import { CompletedListComponent } from './completed-list/completed-list.component';
+import { ParentComponent } from './parent/parent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChoreDetailComponent },
+  { path: 'chore-list', component: ChoreListComponent },
+  { path: 'parent', component: ParentComponent },
+  { path: 'child', component: ChildComponent },
+  { path: 'completed-list', component: CompletedListComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/chores/chore.service.ts
+++ src/app/chores/chore.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
+import { Chore } from "./chore.model";
 
 @Injectable()
-export class Chore {
+export class ChoreService {
 
   private chores: Chore[] = [
     new Chore(
@@ -9,7 +10,7 @@ export class Chore {
       "Complete homework as soon as you get home from school.",
       './src/assets/images/homework.jpg',
       "Today",
-      "0.00",
+       0.00,
     )
   ]
 }
