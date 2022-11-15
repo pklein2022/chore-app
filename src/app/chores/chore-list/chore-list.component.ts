001d0f6d783406521ee47ad37422c953b2ce0adb
pklein2022 <111322242+pklein2022@users.noreply.github.com>
2022-11-14 19:42:47 -0600

    hide delete button from child & fix delete bug

--- src/app/chores/chore-list/chore-list.component.ts
+++ src/app/chores/chore-list/chore-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
+import { DataStorageService } from 'src/app/shared/data-storage.service';
 import { UserService } from 'src/app/users/user.service';
 import { Chore } from '../chore.model';
 import { ChoreService } from '../chore.service';
@@ -13,18 +14,20 @@ import { ChoreService } from '../chore.service';
 export class ChoreListComponent implements OnInit {
   chores: Chore[];
   completedMode = false;
-userRole: string = '';
+  userRole: string = '';
+  dataStorageService: any;
 
   constructor(private choreService: ChoreService,
     private route: ActivatedRoute,
     private router: Router,
     private userService: UserService,
-    private authService: AuthService,) { }
+    private authService: AuthService,
+    private dataService: DataStorageService) { }
 
   ngOnInit(): void {
 
-this.userRole = this.userService.currentUser.role;
-console.log(this.userService.currentUser, "Coming from current user")
+    this.userRole = this.userService.currentUser.role;
+    console.log(this.userService.currentUser, "Coming from current user")
     console.log(this.router.url);
     if (this.router.url === '/completed-chores') {
       this.completedMode = true;
@@ -44,6 +47,8 @@ console.log(this.userService.currentUser, "Coming from current user")
     } else {
       this.chores = this.choreService.getNotCompletedChores();
     }
+    this.dataStorageService.storeChores();
+    this.dataStorageService.fetchChores();
   }
 
   onComplete(id: number) {
